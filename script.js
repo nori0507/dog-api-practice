e1bb52e36ab7da999853110e99023fe941bc31ad
nori0507 <chikyu.rei@ic.edu>
2025-10-29 15:17:44 -0500

    added codes to API

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 console.log("script.js loaded");
 
+async function getDogFact() {
+  const response = await fetch("https://dogapi.dog/api/facts");
+  const data = await response.json();
+  const fact = data.facts[0];
+  
+  // Update the DOM with the dog fact
+  const output = document.getElementById("dog-fact-output"); 
+  output.textContent = fact;
+}
+
+getDogFact()
+
+
+const button = document.getElementById("fetch-dog-btn"); 
+
+button.addEventListener("click", function () { 
+  getDogFact(); 
+});
